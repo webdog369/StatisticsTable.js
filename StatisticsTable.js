22a6fa687942bb9b88b32c75748233b8f2fcd522
webdog369 <1136116938@qq.com>
2020-07-05 19:29:09 +0800

    开发完了条形统计表绘制相关功能

--- StatisticsTable.js
+++ StatisticsTable.js
@@ -69,6 +69,9 @@ class StatisticsTable {
         oCtx.font = `${obj.fontSize}px 微软雅黑`
         // 绘制原点坐标文字
         oCtx.fillText("0",obj.x - 4,obj.y + 2);
+        // 暴露原点坐标
+        this.baseSideX = obj.x;
+        this.baseSideY = obj.y;
         // 绘制x轴坐标文字
         oCtx.fillText(obj.xName,obj.x + this.baseWidth * obj.xLength,obj.y + 5);
         // 绘制y轴坐标文字(实现竖行绘制)
@@ -96,9 +99,40 @@ class StatisticsTable {
         oCtx.stroke();
         oCtx.closePath();
         this.triangle(obj.x,obj.y - this.baseHeight*obj.yLength,false);
+
+        // 暴露x轴和y轴长度 以便绘制条形统计图
+        this.xLength = obj.xLength;
+        this.yLength = obj.yLength;
     }
 
     // 绘制条形图
-
+    bar(obj){
+     // 获取x轴数据
+        let barWidth = (this.baseWidth * this.xLength / obj.xData.length) - 15;
+        let barHeight = this.baseHeight * this.yLength * 2;
+        let baseX = this.baseSideX;
+        let oCtx = this.oCtx;
+        let dataList =obj.xData.map((key,value)=>[key,obj.yData[value]]);
+        let currentBarColor = obj.barColor?obj.barColor:"#333";
+        oCtx.textAlign = "center"
+        oCtx.textBaseline = "middle"
+        oCtx.fillText(obj.title?obj.title:"",obj.titleX?obj.titleX:this.oCanvas.width / 2,obj.titleY?obj.titleY:this.baseHeight*1.5);
+        for (let key of dataList){
+            let linearGradient1 = oCtx.createLinearGradient(baseX + 10,this.baseSideY - (parseFloat(key[1])/100*barHeight),baseX + 10 + barWidth,this.baseSideY - (parseFloat(key[1])/100*barHeight))
+            oCtx.fillStyle = obj.barColor?obj.barColor:"#333";
+            linearGradient1.addColorStop(0,currentBarColor);
+            linearGradient1.addColorStop(.5,"#fff");
+            linearGradient1.addColorStop(1,currentBarColor);
+            obj.linearColor?oCtx.fillStyle = linearGradient1:oCtx.fillStyle = currentBarColor;
+            oCtx.fillRect(baseX + 10,this.baseSideY - (parseFloat(key[1])/100*barHeight) ,barWidth,parseFloat(key[1])/100*barHeight);
+            oCtx.textBaseline = "top"
+            oCtx.font = obj.fontSize?`${obj.fontSize}px 微软雅黑`:"12px 微软雅黑"
+            oCtx.fillStyle = obj.xNameColor?obj.xNameColor:"#000"
+            oCtx.fillText(key[0],baseX + barWidth / 2 + 10,this.baseSideY + 5)
+            oCtx.fillStyle = obj.yNameColor?obj.yNameColor:"#000"
+            oCtx.fillText(key[1],baseX + barWidth / 2 + 10,this.baseSideY - (parseFloat(key[1])/100*barHeight) - 20);
+            baseX += barWidth + 10;
+        }
+    }
 }
 
